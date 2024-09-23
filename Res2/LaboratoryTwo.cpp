6e4fbddd861cfc9bd56fc7741df0094ef9ec46f1
unknown <daniel_jankauskas@outlook.com>
2024-09-23 11:37:13 +0200

    First

---
+++ Res2/LaboratoryTwo.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main()
+{
+    setlocale(LC_CTYPE, "Rus");
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    int i;
+    cout << "Enter the bit number (from 0 to 31): ";
+    cin >> i;
+
+    cout << "Before changing: " << number << endl;
+
+    int mask = ~(1 << i); 
+    int result = number & mask; 
+
+    cout << "After changing: " << result << endl;
+}
